d78e87e979dc1f4b35b6b7c5ed60e9ae1524e0c1
Kevin Almeida <janioklalmeida05@gmail.com>
2022-04-21 16:29:22 -0300

    componentização do elemento .alert-error + teste de senha incorreta + teste de email inválido

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -23,7 +23,78 @@ describe('login', function () {
             loginPage.submit()
 
             dashPage.header.LoggedIn(user.name)
+        })
+    })
 
+    context('quando o usuário é bom mas a senha está incorreta', function () {
+
+        let user = {
+            name: 'User Automated Test',
+            email: 'user@automated.com',
+            password: 'Test@123',
+            is_provider: true
+        }
+
+        before(function () {
+            cy.postUser(user).then(function () {
+                user.password = 'abc123'
+            })
+
+        })
+
+        it('deve notificar erro de credencias', function () {
+            loginPage.go()
+            loginPage.form(user)
+            loginPage.submit()
+
+            const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
+
+            loginPage.toast.shouldHaveText(message)
+        })
+    })
+
+    context('quando o formato do email é inválido', function () {
+
+        const emails = [
+            'user.automated.com',
+            'gmail.com',
+            '@',
+            'user@',
+            '111',
+            '&*^&^&*',
+            'xpto123'
+        ]
+
+        before(function () {
+            loginPage.go()
+        })
+
+        emails.forEach(function (email) {
+            it('não deve logar com o email: ' + email, function () {
+                const user = { email: email, password: 'ped123' }
+
+                loginPage.form(user)
+                loginPage.submit()
+                loginPage.alert.haveText('Informe um email válido')
+            })
+        })
+    })
+
+    context('quando não preencho nenhum dos campos', function () {
+        const alertMessages = [
+            'E-mail é obrigatório',
+            'Senha é obrigatória',
+        ]
+
+        before(function () {
+            loginPage.go()
+            loginPage.submit()
+        })
+
+        alertMessages.forEach(function (alert) {
+            it('deve exibir ' + alert.toLocaleLowerCase(), function () {
+                loginPage.alert.haveText(alert)
+            })
         })
     })
 })
