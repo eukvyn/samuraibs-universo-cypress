bf48a5a3a889dca9bc1fcd6cc57e8118cf8a7ab6
Kevin Almeida <janioklalmeida05@gmail.com>
2022-04-21 21:06:25 -0300

    pegando o token de login da api

---
+++ cypress/integration/dash.spec.js
@@ -0,0 +1,51 @@
+
+
+describe('dashboard', function () {
+
+    context('quando o cliente faz um agendamento no app mobile', function () {
+
+        const data = {
+            costumer: {
+                name: 'Nikki Sixx',
+                email: 'sixx@motleycrue.com',
+                password: 'pwd123',
+                is_provider: false
+            },
+            samurai: {
+                name: 'Ramon Valdes',
+                email: 'ramon@televisa.com',
+                password: 'pwd123',
+                is_provider: true
+            }
+        }
+
+        before(function () {
+            cy.postUser(data.costumer)
+            cy.postUser(data.samurai)
+
+            cy.apiLogin(data.costumer)
+            cy.log('Take this vision meu parceiro: ' + Cypress.env('apiToken'))
+        })
+
+        it('o mesmo deve ser exibido no dashboard', function () {
+            console.log(data)
+        })
+    })
+})
+
+Cypress.Commands.add('apiLogin', function (user) {
+
+    const payload = {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/sessions',
+        body: payload
+    }).then(function (response){
+        expect(response.status).to.eq(200)
+        Cypress.env('apiToken', response.body.token)
+    })
+})
